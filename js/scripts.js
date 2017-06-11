daa66581a31214419e7ffe0138e688b2df97eab5
Calla Rudolph <callarudolph@gmail.com>
2017-06-11 11:55:27 -0700

    create var for string length, if statement for invalid entry

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,25 @@
 //Business logic here:
-var roman = function(number) {
-
-  var symbols = ["I", "V", "X", "L", "C", "D", "M"];
-  var singleDigitNumber = false;
-
-    if (number <= 9) {
-      singleDigitNumber = true;
-    }
-
-    if (singleDigitNumber === false && number < 50) {
-      var numberTenToFortynine = true;
-    }
-
-    if (number < 4) {
-      return "I".repeat(number);
-    } else if (number === 4) {
-      return "IV";
-    } else if (number === 5) {
-      return "V";
-    } else if (number > 5 &&  number < 9) {
-      return "V" + "I".repeat(number % 5);
-    } else if (number === 9) {
-      return "IX";
-    }
-
-    if (number === 10) {
-      return "X";
-    } else if (number > 10 && number < 14) {
-      return "X" + "I".repeat(number % 5);
-    } else if (number === 14) {
-      return "XIV";
-    } else if (number === 15) {
-      return "XV";
-    } else if (number > 15 && number < 19) {
-      return "XV" + "I".repeat(number % 5);
-    } else if (number === 19) {
-      return "XIX";
-    } else if (number === 20) {
-      return "XX";
-    }
-
-
-  // if (number === 1) {
-  //   return symbols[0];
-  // } else if (number === 5) {
-  //   return symbols[1];
-  // } else if (number === 10) {
-  //   return symbols[2];
-  // } else if (number === 50) {
-  //   return symbols[3];
-  // } else if (number === 100) {
-  //   return symbols[4];
-  // } else if (number === 500) {
-  //   return symbols[5];
-  // } else if (number === 1000) {
-  //   return symbols[6];
-  // } else {
-  //   return "error";
-  // }
-};
+// var thousandsPlace = ((string.length) - 4);
+// var hundredsPlace = ((string.length) - 3);
+// var tensPlace = ((string.length) - 2);
+// var onesPlace = ((string.length) - 1);
 
 
 
 
 
-
-
-
-
-
-//Ui logic here:
+//UI logic here:
 $(document).ready(function() {
   $("form#romanNumeral").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("#input").val());
-    var result = roman(number);
+    var string = number.toString();
+    var result = "";
+
+    if (number >= 4000 || number <= 0) {
+      result = "Number invalid, Roman Numerals only count from 1 to 3,999."
+    }
+    // var result = roman(number);
     $("#result").text(result);
   });
 });
